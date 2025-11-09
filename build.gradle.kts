47710fad9ec721f84fb19199e5339ac5e75bbbcf
EndyHell <anderhellstorm@gmail.com>
2025-11-09 04:40:34 +0300

    Обновление до 1.21.8.

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
 }
 
